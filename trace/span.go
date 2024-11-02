26d9eea50177c4469bac218987efadba6d396104
Caio Felix <caiohalcsik@gmail.com>
2024-11-02 13:48:16 -0300

    feat: working in progress

--- trace/span.go
+++ trace/span.go
@@ -10,6 +10,9 @@ import (
 
 func WithSpan(ctx context.Context, fn func(ctx context.Context, span trace.Span) (any, error)) (any, error) {
 	tracer := otel.GetTracerProvider()
+	if tracer == nil {
+		return fn(ctx, nil)
+	}
 	pc, _, _, _ := runtime.Caller(1)
 	fullFuncName := runtime.FuncForPC(pc).Name()
 	ctx, span := tracer.Tracer(GlobalTracer.opts.Name).Start(ctx, fullFuncName)
